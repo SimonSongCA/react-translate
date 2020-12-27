3f5df922ba45dd9c72c39b4a1593c0d9e97e15f3
Simon <>
2020-12-26 18:16:42 -0800

    Replaced 'Context' with 'Consumer' in BUtton.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,12 +2,26 @@ import React from "react";
 import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends React.Component {
-  // set up the property of language to the current Button object
-  static contextType = LanguageContext;
+  // // set up the property of language to the current Button object
+  // static contextType = LanguageContext;
 
+  // render() {
+  //   const text = this.context === "english" ? "Submit" : "Voorleggen";
+  //   return <button className="ui button primary">{text}</button>;
+  // }
+
+  // replace 'Context' with 'Consumer'
+  renderSubmit(value) {
+    return value === "english" ? "Submit" : "Voorleggen";
+  }
   render() {
-    const text = this.context === "english" ? "Submit" : "Voorleggen";
-    return <button className="ui button primary">{text}</button>;
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {(value) => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 // an alternative way to set up the contextType.
